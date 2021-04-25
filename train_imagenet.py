5f4b76244823c638f3dc99d640bacc948c12b1e7
MarcAntoineAlex <18510756258@163.com>
2021-04-25 15:42:04 +0800

    sth

--- train_imagenet.py
+++ train_imagenet.py
@@ -47,7 +47,6 @@ args, unparsed = parser.parse_known_args()
 jobid = os.environ["SLURM_JOBID"]
 args.save = '{}/{}'.format(args.save, jobid)
 utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
-
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
     format=log_format, datefmt='%m/%d %I:%M:%S %p')
